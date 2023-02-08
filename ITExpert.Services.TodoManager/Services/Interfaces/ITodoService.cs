166c4b0797fec84964f81fd92966f731ccde5852
ranenko <ranenko@ekosphere.pro>
2023-02-09 00:14:46 +0700

    TodoService implementation added

--- ITExpert.Services.TodoManager/Services/Interfaces/ITodoService.cs
+++ ITExpert.Services.TodoManager/Services/Interfaces/ITodoService.cs
@@ -5,12 +5,12 @@ namespace ITExpert.Services.TodoManager.Services.Interfaces
 {
     public interface ITodoService
     {
-        public Task<Dictionary<Todo, string>> GetTodosWithHashAsync();
-        public Task<bool> CreateTodoAsync(string title, TodoCategory category, TodoColor color);
+        public Task<List<Todo>> GetTodosWithHashAsync();
+        public Task CreateTodoAsync(string title, TodoCategory category, TodoColor color);
         public Task<Todo> GetTodoAsync(int id);
-        public Task<bool> DeleteTodoAsync(int id);
-        public Task<bool> UpdateTodoTitleAsync(int id, string title);
+        public Task DeleteTodoAsync(int id);
+        public Task UpdateTodoTitleAsync(int id, string title);
         public Task<List<Comment>> GetTodoCommentsAsync(int id);
-        public Task<bool> AddTodoCommentAsync(int id, Comment comment);
+        public Task AddTodoCommentAsync(int id, string comment);
     }
 }
--- ITExpert.Services.TodoManager/Services/TodoService.cs
+++ ITExpert.Services.TodoManager/Services/TodoService.cs
@@ -1,7 +1,9 @@
 ﻿using ITExpert.Libraries.SharedLibrary.Enums;
+using ITExpert.Libraries.SharedLibrary.Exceptions;
 using ITExpert.Libraries.SharedLibrary.Models.DAO;
 using ITExpert.Services.TodoManager.DAL.DbContexts;
 using ITExpert.Services.TodoManager.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace ITExpert.Services.TodoManager.Services
 {
@@ -16,39 +18,88 @@ namespace ITExpert.Services.TodoManager.Services
             _todoDbContext = todoDbContext;
         }
 
-        public Task<bool> AddTodoCommentAsync(int id, Comment comment)
+        public async Task AddTodoCommentAsync(int id, string comment)
         {
-            throw new NotImplementedException();
+            var findedTodo = await _todoDbContext.Todos
+                .Include(todo => todo.Comments)
+                .FirstOrDefaultAsync(todo => todo.Id == id);
+
+            if (findedTodo == null)
+                throw new EntityNotFoundException($"Adding new comment to Todo failed. Todo with id='{id}' was not found in database.");
+
+            if (findedTodo.Comments.FirstOrDefault(c => c.Text.Equals(comment)) is not null)
+                throw new ArgumentException($"Comment '{comment}' already exists in Todo with id='{id}'.");
+
+            findedTodo.Comments.Add(new Comment { Text = comment });
+            await _todoDbContext.SaveChangesAsync();
         }
 
-        public Task<bool> CreateTodoAsync(string title, TodoCategory category, TodoColor color)
+        public async Task CreateTodoAsync(string title, TodoCategory category, TodoColor color)
         {
-            throw new NotImplementedException();
+            var newTodo = new Todo
+            {
+                Title = title,
+                Category = category,
+                Color = color
+            };
+
+            await _todoDbContext.Todos.AddAsync(newTodo);
+            await _todoDbContext.SaveChangesAsync();
         }
 
-        public Task<bool> DeleteTodoAsync(int id)
+        public async Task DeleteTodoAsync(int id)
         {
-            throw new NotImplementedException();
+            var todoForDeleting = new Todo { Id = id };
+
+            _todoDbContext.Todos.Attach(todoForDeleting);
+            _todoDbContext.Remove(todoForDeleting);
+            await _todoDbContext.SaveChangesAsync();
         }
 
-        public Task<Todo> GetTodoAsync(int id)
+        public async Task<Todo> GetTodoAsync(int id)
         {
-            throw new NotImplementedException();
+            var findedTodo = await _todoDbContext.Todos
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(todo => todo.Id == id);
+
+            if (findedTodo is null)
+                throw new EntityNotFoundException($"Todo with id='{id}' was not found.");
+
+            return findedTodo;
         }
 
-        public Task<List<Comment>> GetTodoCommentsAsync(int id)
+        public async Task<List<Comment>> GetTodoCommentsAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _todoDbContext.Comments
+                .AsNoTracking()
+                .Where(comment => comment.TodoId == id)
+                .ToListAsync();
         }
 
-        public Task<Dictionary<Todo, string>> GetTodosWithHashAsync()
+        public async Task<List<Todo>> GetTodosWithHashAsync()
         {
-            throw new NotImplementedException();
+            return await _todoDbContext.Todos
+                .AsNoTracking()
+                .Include(todo => todo.Comments)
+                .ToListAsync();
         }
 
-        public Task<bool> UpdateTodoTitleAsync(int id, string title)
+        public async Task UpdateTodoTitleAsync(int id, string title)
         {
-            throw new NotImplementedException();
+            var findedTodo = await _todoDbContext.Todos
+                .Select(todo => new Todo
+                {
+                    Id = todo.Id,
+
+                })
+                .FirstOrDefaultAsync(todo => todo.Id == id);
+
+            if (findedTodo is null)
+                throw new EntityNotFoundException($"Updating Todo title failed. Todo with id='{id}' was not found in database.");
+
+            findedTodo.Title = title;
+
+            await _todoDbContext.SaveChangesAsync();
         }
     }
 }
