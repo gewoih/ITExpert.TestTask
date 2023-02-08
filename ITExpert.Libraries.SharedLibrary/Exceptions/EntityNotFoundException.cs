3b1d044ca809ac9761642ca93f29c9d6c1d2c420
ranenko <ranenko@ekosphere.pro>
2023-02-09 00:13:29 +0700

    Custom exception added

---
+++ ITExpert.Libraries.SharedLibrary/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace ITExpert.Libraries.SharedLibrary.Exceptions
+{
+    public sealed class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException()
+        {
+        }
+
+        public EntityNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public EntityNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public EntityNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
