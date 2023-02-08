5072867475d1ca223433291ff02c4a9480d9c261
ranenko <ranenko@ekosphere.pro>
2023-02-08 16:05:15 +0700

    TodoService added

---
+++ ITExpert.Services.TodoManager/Services/Interfaces/ITodoService.cs
@@ -0,0 +1,16 @@
+﻿using ITExpert.Libraries.SharedLibrary.Enums;
+using ITExpert.Libraries.SharedLibrary.Models.DAO;
+
+namespace ITExpert.Services_.Services.Interfaces
+{
+    public interface ITodoService
+    {
+        public Task<Dictionary<Todo, string>> GetTodosWithHashAsync();
+        public Task<bool> CreateTodoAsync(string title, TodoCategory category, TodoColor color);
+        public Task<Todo> GetTodoAsync(int id);
+        public Task<bool> DeleteTodoAsync(int id);
+        public Task<bool> UpdateTodoTitleAsync(int id, string title);
+        public Task<List<Comment>> GetTodoCommentsAsync(int id);
+        public Task<bool> AddTodoCommentAsync(int id, Comment comment);
+    }
+}
---
+++ ITExpert.Services.TodoManager/Services/TodoService.cs
@@ -0,0 +1,44 @@
+﻿using ITExpert.Libraries.SharedLibrary.Enums;
+using ITExpert.Libraries.SharedLibrary.Models.DAO;
+using ITExpert.Services_.Services.Interfaces;
+
+namespace ITExpert.Services_.Services
+{
+    public sealed class TodoService : ITodoService
+    {
+        public Task<bool> AddTodoCommentAsync(int id, Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateTodoAsync(string title, TodoCategory category, TodoColor color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTodoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Todo> GetTodoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Comment>> GetTodoCommentsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<Todo, string>> GetTodosWithHashAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTodoTitleAsync(int id, string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
